e089923bf2b1feb2b4e9bc0eb4077b81c621d551
JacksonOR <oli.jackson@gmail.com>
2020-05-03 19:31:35 +0100

    updated registration system, incomplete

--- application.py
+++ application.py
@@ -6,9 +6,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from wtform_fields import *
+from models import *
 
+# Configure app
 app = Flask(__name__)
 app.secret_key = 'CHANGE-LATER'
+
+# Configure database - THIS IS NOT VERY SECURE TO LEAVE DB INFO HERE
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://xoikhwtplbrmgh:7f40ed19a11bc14c9c1592561e6e220cf3006af8a3202cef51381ee4b49f2462@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/d2tja8a5tiraod'
+db = SQLAlchemy(app)
+
 
 # Check for environment variable - harvard starter project 1
 if not os.getenv("DATABASE_URL"):
@@ -28,6 +35,20 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        #check if username already exists
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "This username is already taken."
+
+        #but if username is available.... (first value is name of the column, second value is the user input from the form)
+        #Add the user to the DB...
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Inserted into DB!"
         return "Welcome to the club! Registration successful."
     return render_template("index.html", form=reg_form)
 
--- wtform_fields.py
+++ wtform_fields.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField("username_label", validators=[InputRequired(message="Username required"), Length(min=4, max=20, message="Username must be between 4 and 20 characters")])
-    password = PasswordField("password_label", validators=[InputRequired(message="Password required"), Length(min=4, max=20, message="Password must be between 4 and 20 characters")])
+    password = PasswordField("password_label", validators=[InputRequired(message="Password required")])
     confirm_password = PasswordField("confirm_password_label", validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
     submit_button = SubmitField('Register')
 
