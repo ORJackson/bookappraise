fcc4158cc4d714c2cfc714320dab1ea08f920cb4
JacksonOR <oli.jackson@gmail.com>
2020-05-25 22:28:34 +0200

    began using goodreads API

--- application.py
+++ application.py
@@ -2,27 +2,32 @@ import os
 
 from flask import Flask, session, render_template, redirect, url_for, request, flash
 from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_login import LoginManager, login_user, current_user, logout_user
+
+from flask_sqlalchemy import SQLAlchemy 
 
 from wtform_fields import *
 from models import *
 from tables import Results
 
 import string
+import requests
 
 # secret_key and database info stored in .env
 # Configure app
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', None)
 
-# Configure database
-app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('SQLALCHEMY_DATABASE_URI')
+# Configure database - using Flask-SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
-engine = create_engine(os.getenv("DATABASE_URL"))
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+#My config using SQLAlchemy:
+
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 # Set up database - provided in harvard starter code project 1 - CAUSES ERROR
 # engine = create_engine(os.getenv("DATABASE_URL"))
@@ -60,8 +65,11 @@ def index():
         hashed_password = pbkdf2_sha256.hash(password)
         
         #Add the user to the DB... (first value is name of the column, second value is the user input from the form)
-        user = User(username=username, password=hashed_password)
-        db.session.add(user)
+        # user = User(username=username, password=hashed_password)
+        # db.session.add(user)
+        # db.session.commit()
+
+        db.session.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": username, "password" : hashed_password})
         db.session.commit()
 
         return redirect(url_for('login'))
@@ -74,12 +82,20 @@ def login():
 
     login_form = LoginForm()
 
-    #Allow login if validation is successful
+    # Allow login if validation is successful
     if login_form.validate_on_submit():
         user_object = User.query.filter_by(username=login_form.username.data).first()
         login_user(user_object)
         return redirect(url_for('search'))
+
+    #Allow login if validation is successful - ERROR
+    # username = login_form.username.data
        
+    # if login_form.validate_on_submit():
+    #     user_object = db.session.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
+    #     login_user(user_object)
+    #     return redirect(url_for('search'))
+
     return render_template("login.html", form=login_form)
 
 
@@ -95,18 +111,25 @@ def search():
     return render_template("search.html", form=search)
 
 
-@app.route('/results')
+@app.route('/results', methods=['GET'])
 def search_results(search):
     results = []
     search_string = string.capwords(search.data['search'], sep = None)
 
     if search_string:
-        qry = db_session.query(Book).filter((Book.title.contains(search_string)) | (Book.author.contains(search_string)) | (Book.year.contains(search_string)) | (Book.isbn.contains(search_string)))
-        results = qry.order_by(Book.author.asc()).all()
+        # qry = db_session.query(Book).filter((Book.title.contains(search_string)) | (Book.author.contains(search_string)) | (Book.year.contains(search_string)) | (Book.isbn.contains(search_string)))
+        # qry = db.session.query(Book).filter((Book.title.contains(search_string)) | (Book.author.contains(search_string)) | (Book.year.contains(search_string)) | (Book.isbn.contains(search_string)))
+        # results = qry.order_by(Book.author.asc()).all()
+
+        results = db.session.execute("SELECT isbn, author, title, year FROM books WHERE isbn iLIKE '%"+search_string+"%' OR author iLIKE '%"+search_string+"%' OR title iLIKE '%"+search_string+"%' OR year iLIKE '%"+search_string+"%' ORDER BY author").fetchall()
+        # results = qry.order_by(Book.author.asc()).all() 
+          
 
     else:
-        qry = db_session.query(Book)
-        results = qry.all()
+        # qry = db_session.query(Book)
+        # qry = db.session.query(Book)
+        # results = qry.all()
+        results = db.session.execute("SELECT * FROM books ORDER BY year")
         
     if not results:
         flash('No results found!')
@@ -116,6 +139,38 @@ def search_results(search):
         table = Results(results)
         table.border = True
         return render_template('results.html', table=table)
+
+# @app.route('/book', methods=['GET'])
+# def display_info():
+#     if not current_user.is_authenticated:
+#         return render_template("please_login.html")
+#     else:
+#         return render_template("book.html")
+
+@app.route('/book/<isbn>', methods=['GET'])
+def display_info(isbn):
+    if not current_user.is_authenticated:
+        return render_template("please_login.html")
+    else:
+        if request.method == 'GET':
+
+            row = db.session.execute("SELECT isbn, title, author, year FROM books WHERE \
+                            isbn = :isbn",
+                            {"isbn": isbn})
+            book_info = row.fetchone()
+
+            """USE GOODREADS API"""
+
+            key = os.getenv('GOODREADS_API_KEY')
+
+            res = requests.get("https://www.goodreads.com/book/review_counts.json", params = {"key" : key, "isbns": isbn})
+            if res.status_code != 200:
+                return render_template("error.html", message="API request unsuccessful.")
+            review_data = res.json()
+
+            return render_template("book.html", book_info = book_info, review_data = review_data)
+        
+
 
 
 @app.route("/logout", methods=['GET'])
--- tables.py
+++ tables.py
@@ -1,9 +1,12 @@
-from flask_table import Table, Col
+from flask_table import Table, Col, LinkCol
+from models import Book
 
 class Results(Table):
     id = Col('Id', show=False)
     isbn = Col('ISBN')
+    # isbn = LinkCol('ISBN', endpoint='display_info', attr=(Book.isbn))
+    # isbn = LinkCol('ISBN', endpoint='display_info', url_kwargs=dict(id='id'), anchor_attrs={Book.isbn})
     title = Col('Title')
     author = Col('Author')
     year = Col('Year')
-    
+    more_info = LinkCol('More Info',   url_kwargs=dict(isbn='isbn'), endpoint='display_info')
